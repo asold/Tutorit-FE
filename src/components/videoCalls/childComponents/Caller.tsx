b96360dea463d221d4487eb23bf31f9af0171860
asold <asoldan@mail.ru>
2024-07-20 18:37:26 +0200

    Added some small functionality to the calling process - using objects/classes

--- src/components/videoCalls/childComponents/Caller.tsx
+++ src/components/videoCalls/childComponents/Caller.tsx
@@ -1,10 +1,11 @@
-import React, { useRef, useEffect, useState, useCallback } from 'react';
+// Caller Component
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { ThunkDispatch } from 'redux-thunk';
 import { AnyAction } from 'redux';
+import SignalRHandler from '../../../common/signalRHandler.ts';
 import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
-import { MessagePackHubProtocol } from '@microsoft/signalr-protocol-msgpack';
-import { SERVER_ADDRESS } from '../../../common/constants.ts';
+
 
 const Caller = ({ token }) => {
     const videoRef = useRef<HTMLVideoElement>(null);
@@ -16,41 +17,34 @@ const Caller = ({ token }) => {
     const [callPartnerUsername, setCallPartnerUsername] = useState('');
     const [mediaRecorder, setMediaRecorder] = useState<MediaRecorder | null>(null);
     const [receiverCallAccepted, setReceiverCallAccepted] = useState(false);
-    const [key, setKey] = useState(0); // Add a key state to force re-render
+    const [key, setKey] = useState(0);
 
-    const isReceiving = useSelector((state: any) => state.receiver.isReceiving); // Get isReceiving state from Redux
-    const initialCallerUserName = useSelector((state:any)=> state.receiver.initialCallerUserName);
+    const isReceiving = useSelector((state: any) => state.receiver.isReceiving);
+    const initialCallerUserName = useSelector((state: any) => state.receiver.initialCallerUserName);
+
+    const signalRHandler = new SignalRHandler();
 
     const handlePartnerUsernameChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
         setCallPartnerUsername(e.target.value);
     }, []);
 
-    const initializeConnection = useCallback(() => {
+    const initializeConnection = useCallback(async () => {
         if (!connection && !isConnecting) {
             setIsConnecting(true);
-            const userToken = encodeURIComponent(token);
-            const connect = new HubConnectionBuilder()
-                .withUrl(`${SERVER_ADDRESS}/hub?userToken=${encodeURIComponent(token)}&connectionType=${0}`)
-                .withAutomaticReconnect()
-                .withHubProtocol(new MessagePackHubProtocol())
-                .build();
-
-            connect.start().then(() => {
-                console.log('SignalR connection established in Sender');
+            try {
+                const connect = await signalRHandler.createSignalRConnection(0, token);
                 setConnection(connect);
                 setIsConnecting(false);
 
-                // Listen for the 'callaccepted' event
-                connect.on('callaccepted', () => {
-                    //Step 2: Sender informed that call is accepted
+                signalRHandler.onConnectionEvent(connect, 'callaccepted', () => {
                     console.log('Call accepted by receiver');
-                    setReceiverCallAccepted(true); // this starts sending!!
+                    setReceiverCallAccepted(true);
                 });
 
-            }).catch(err => {
+            } catch (err) {
                 console.error('Error while establishing SignalR connection:', err);
                 setIsConnecting(false);
-            });
+            }
         }
     }, [connection, isConnecting, token]);
 
@@ -58,10 +52,7 @@ const Caller = ({ token }) => {
         initializeConnection();
         return () => {
             if (connection) {
-                connection.stop().then(() => {
-                    console.log('SignalR connection closed');
-                    setConnection(null);
-                });
+                signalRHandler.stopConnection(connection);
             }
         };
     }, [connection, initializeConnection]);
@@ -78,16 +69,13 @@ const Caller = ({ token }) => {
             if (event.data && event.data.size > 0 && connection && connection.state === 'Connected') {
                 const arrayBuffer = await event.data.arrayBuffer();
                 try {
-                        if(connectionType === "ReceiveVideoStream"){
-                            await connection.send('ReceiveVideoStream', callPartnerUsername, new Uint8Array(arrayBuffer));
-                            console.log('Chunk of data sent to' , 'ReceiveVideoStream');
-
-                        }                        
-                        else if(connectionType === "SendVideoToSender"){
-                            await connection.send('SendVideoToSender', initialCallerUserName, new Uint8Array(arrayBuffer), "TESTING");
-                            console.log('Chunk of data sent to' , 'SendVideoToSender');
-
-                        }
+                    if (connectionType === "ReceiveVideoStream") {
+                        await signalRHandler.sendMessageThroughConnection(connection, 'ReceiveVideoStream', callPartnerUsername, new Uint8Array(arrayBuffer));
+                        console.log('Chunk of data sent to ReceiveVideoStream');
+                    } else if (connectionType === "SendVideoToSender") {
+                        await signalRHandler.sendMessageThroughConnection(connection, 'SendVideoToSender', initialCallerUserName, new Uint8Array(arrayBuffer), "TESTING");
+                        console.log('Chunk of data sent to SendVideoToSender');
+                    }
                 } catch (error) {
                     console.error('Error sending video chunk:', error);
                 }
@@ -128,74 +116,55 @@ const Caller = ({ token }) => {
 
     const handleStartCameraClick = useCallback(async () => {
         if (connection) {
-            //Step 1 - requesting permission to call
-            await connection.send('RequestCallUser', callPartnerUsername); // Custom method on server
+            await signalRHandler.sendMessageThroughConnection(connection, 'RequestCallUser', callPartnerUsername);
         }
     }, [connection, callPartnerUsername]);
-
 
     useEffect(() => {
         if (receiverCallAccepted) {
             console.log('Starting camera after Call Accepted!!');
-            //Step 3: Sender starts sending. 
             startCamera('ReceiveVideoStream');
-            // startCamera('SendVideoToSender');
         }
     }, [receiverCallAccepted, startCamera]);
 
     useEffect(() => {
         if (isReceiving) {
             console.log('Starting camera after isReceiving is True!!!');
-            //Step 3: Sender starts sending. 
             startCamera('SendVideoToSender');
         }
     }, [isReceiving, initialCallerUserName, startCamera]);
 
-
-
-
     const handleStopCameraClick = useCallback(async () => {
         console.log('Stop camera click');
-
-        // Stop the media recorder if it's recording
         if (mediaRecorder && mediaRecorder.state === "recording") {
             mediaRecorder.stop();
             console.log('MediaRecorder stopped');
         }
 
-        // Stop all tracks from the video stream
         if (videoStream) {
             videoStream.getTracks().forEach(track => {
                 console.log('Stopping track:', track.kind);
                 track.stop();
             });
-            setVideoStream(null); // Clear the video stream
+            setVideoStream(null);
         }
 
-        // Notify the server that the client is about to disconnect
         if (connection) {
             try {
-                await connection.send('PrepareForDisconnection'); // Custom method on server
+                await signalRHandler.sendMessageThroughConnection(connection, 'PrepareForDisconnection');
                 console.log('Server notified for disconnection');
             } catch (error) {
                 console.error('Error notifying server:', error);
             }
 
             console.log('Closing SignalR connection...');
-            await connection.stop()
-                .then(() => {
-                    console.log('SignalR connection closed');
-                    setConnection(null); // Reset connection state to null
-                })
-                .catch(error => {
-                    console.error('Failed to close SignalR connection:', error);
-                });
+            await signalRHandler.stopConnection(connection);
+            setConnection(null);
         }
 
-        // Reset the call accepted state and refresh the component
         setReceiverCallAccepted(false);
-        setKey(prevKey => prevKey + 1); // Update the key to force re-render
-    }, [mediaRecorder, videoStream, connection, initializeConnection]);
+        setKey(prevKey => prevKey + 1);
+    }, [mediaRecorder, videoStream, connection]);
 
     return (
         <div key={key}>
@@ -212,3 +181,220 @@ const Caller = ({ token }) => {
 };
 
 export default Caller;
+
+
+
+// import React, { useRef, useEffect, useState, useCallback } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { ThunkDispatch } from 'redux-thunk';
+// import { AnyAction } from 'redux';
+// import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
+// import { MessagePackHubProtocol } from '@microsoft/signalr-protocol-msgpack';
+// import { SERVER_ADDRESS } from '../../../common/constants.ts';
+
+// const Caller = ({ token }) => {
+//     const videoRef = useRef<HTMLVideoElement>(null);
+//     const [videoStream, setVideoStream] = useState<MediaStream | null>(null);
+//     const [connection, setConnection] = useState<HubConnection | null>(null);
+//     const [isConnecting, setIsConnecting] = useState(false);
+//     const [isRecording, setIsRecording] = useState(false);
+//     const dispatch = useDispatch<ThunkDispatch<any, any, AnyAction>>();
+//     const [callPartnerUsername, setCallPartnerUsername] = useState('');
+//     const [mediaRecorder, setMediaRecorder] = useState<MediaRecorder | null>(null);
+//     const [receiverCallAccepted, setReceiverCallAccepted] = useState(false);
+//     const [key, setKey] = useState(0); // Add a key state to force re-render
+
+//     const isReceiving = useSelector((state: any) => state.receiver.isReceiving); // Get isReceiving state from Redux
+//     const initialCallerUserName = useSelector((state:any)=> state.receiver.initialCallerUserName);
+
+//     const handlePartnerUsernameChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+//         setCallPartnerUsername(e.target.value);
+//     }, []);
+
+//     const initializeConnection = useCallback(() => {
+//         if (!connection && !isConnecting) {
+//             setIsConnecting(true);
+//             const userToken = encodeURIComponent(token);
+//             const connect = new HubConnectionBuilder()
+//                 .withUrl(`${SERVER_ADDRESS}/hub?userToken=${encodeURIComponent(token)}&connectionType=${0}`)
+//                 .withAutomaticReconnect()
+//                 .withHubProtocol(new MessagePackHubProtocol())
+//                 .build();
+
+//             connect.start().then(() => {
+//                 console.log('SignalR connection established in Sender');
+//                 setConnection(connect);
+//                 setIsConnecting(false);
+
+//                 // Listen for the 'callaccepted' event
+//                 connect.on('callaccepted', () => {
+//                     //Step 2: Sender informed that call is accepted
+//                     console.log('Call accepted by receiver');
+//                     setReceiverCallAccepted(true); // this starts sending!!
+//                 });
+
+//             }).catch(err => {
+//                 console.error('Error while establishing SignalR connection:', err);
+//                 setIsConnecting(false);
+//             });
+//         }
+//     }, [connection, isConnecting, token]);
+
+//     useEffect(() => {
+//         initializeConnection();
+//         return () => {
+//             if (connection) {
+//                 connection.stop().then(() => {
+//                     console.log('SignalR connection closed');
+//                     setConnection(null);
+//                 });
+//             }
+//         };
+//     }, [connection, initializeConnection]);
+
+//     const startRecording = useCallback((stream, connectionType) => {
+//         const options = { mimeType: 'video/webm; codecs=vp8' };
+//         if (!MediaRecorder.isTypeSupported(options.mimeType)) {
+//             console.error(`${options.mimeType} is not supported`);
+//             return;
+//         }
+
+//         const recorder = new MediaRecorder(stream, options);
+//         recorder.ondataavailable = async (event) => {
+//             if (event.data && event.data.size > 0 && connection && connection.state === 'Connected') {
+//                 const arrayBuffer = await event.data.arrayBuffer();
+//                 try {
+//                         if(connectionType === "ReceiveVideoStream"){
+//                             await connection.send('ReceiveVideoStream', callPartnerUsername, new Uint8Array(arrayBuffer));
+//                             console.log('Chunk of data sent to' , 'ReceiveVideoStream');
+
+//                         }                        
+//                         else if(connectionType === "SendVideoToSender"){
+//                             await connection.send('SendVideoToSender', initialCallerUserName, new Uint8Array(arrayBuffer), "TESTING");
+//                             console.log('Chunk of data sent to' , 'SendVideoToSender');
+
+//                         }
+//                 } catch (error) {
+//                     console.error('Error sending video chunk:', error);
+//                 }
+//             }
+//         };
+
+//         recorder.onerror = (error) => console.error('MediaRecorder error:', error);
+//         recorder.onstart = () => {
+//             console.log('MediaRecorder started');
+//             setIsRecording(true);
+//         };
+//         recorder.onstop = () => {
+//             console.log('MediaRecorder stopped');
+//             setIsRecording(false);
+//         };
+
+//         recorder.start(100);
+//         setMediaRecorder(recorder);
+//     }, [connection, callPartnerUsername, initialCallerUserName]);
+
+//     const startCamera = useCallback(async (connectionType) => {
+//         if (!isRecording) {
+//             console.log('Attempting to access camera...');
+//             const constraints = { video: true };
+//             try {
+//                 const stream = await navigator.mediaDevices.getUserMedia(constraints);
+//                 setVideoStream(stream);
+//                 if (videoRef.current) {
+//                     videoRef.current.srcObject = stream;
+//                 }
+//                 console.log('Camera feed set to video element');
+//                 startRecording(stream, connectionType);
+//             } catch (error) {
+//                 console.error('Error accessing camera:', error);
+//             }
+//         }
+//     }, [isRecording, startRecording]);
+
+//     const handleStartCameraClick = useCallback(async () => {
+//         if (connection) {
+//             //Step 1 - requesting permission to call
+//             await connection.send('RequestCallUser', callPartnerUsername); // Custom method on server
+//         }
+//     }, [connection, callPartnerUsername]);
+
+
+//     useEffect(() => {
+//         if (receiverCallAccepted) {
+//             console.log('Starting camera after Call Accepted!!');
+//             //Step 3: Sender starts sending. 
+//             startCamera('ReceiveVideoStream');
+//             // startCamera('SendVideoToSender');
+//         }
+//     }, [receiverCallAccepted, startCamera]);
+
+//     useEffect(() => {
+//         if (isReceiving) {
+//             console.log('Starting camera after isReceiving is True!!!');
+//             //Step 3: Sender starts sending. 
+//             startCamera('SendVideoToSender');
+//         }
+//     }, [isReceiving, initialCallerUserName, startCamera]);
+
+
+
+
+//     const handleStopCameraClick = useCallback(async () => {
+//         console.log('Stop camera click');
+
+//         // Stop the media recorder if it's recording
+//         if (mediaRecorder && mediaRecorder.state === "recording") {
+//             mediaRecorder.stop();
+//             console.log('MediaRecorder stopped');
+//         }
+
+//         // Stop all tracks from the video stream
+//         if (videoStream) {
+//             videoStream.getTracks().forEach(track => {
+//                 console.log('Stopping track:', track.kind);
+//                 track.stop();
+//             });
+//             setVideoStream(null); // Clear the video stream
+//         }
+
+//         // Notify the server that the client is about to disconnect
+//         if (connection) {
+//             try {
+//                 await connection.send('PrepareForDisconnection'); // Custom method on server
+//                 console.log('Server notified for disconnection');
+//             } catch (error) {
+//                 console.error('Error notifying server:', error);
+//             }
+
+//             console.log('Closing SignalR connection...');
+//             await connection.stop()
+//                 .then(() => {
+//                     console.log('SignalR connection closed');
+//                     setConnection(null); // Reset connection state to null
+//                 })
+//                 .catch(error => {
+//                     console.error('Failed to close SignalR connection:', error);
+//                 });
+//         }
+
+//         // Reset the call accepted state and refresh the component
+//         setReceiverCallAccepted(false);
+//         setKey(prevKey => prevKey + 1); // Update the key to force re-render
+//     }, [mediaRecorder, videoStream, connection, initializeConnection]);
+
+//     return (
+//         <div key={key}>
+//             <h1>Video Interaction</h1>
+//             <div>
+//                 <label htmlFor="username">Username:</label>
+//                 <input type="text" id="username" value={callPartnerUsername} onChange={handlePartnerUsernameChange} required />
+//                 <button onClick={handleStartCameraClick} disabled={isConnecting || isRecording || receiverCallAccepted || isReceiving || !callPartnerUsername}>Call Person</button>
+//                 <button onClick={handleStopCameraClick} disabled={isConnecting || !isRecording}>Stop Call</button>
+//             </div>
+//             <video ref={videoRef} autoPlay playsInline />
+//         </div>
+//     );
+// };
+
+// export default Caller;
--- src/components/videoCalls/childComponents/SignalRHandler.tsx
+++ src/components/videoCalls/childComponents/SignalRHandler.tsx
@@ -1,69 +1,56 @@
-import { HubConnection, HubConnectionBuilder, LogLevel } from '@microsoft/signalr';
-import { MessagePackHubProtocol } from '@microsoft/signalr-protocol-msgpack';
 import React, { useEffect, useState } from 'react';
-import CallAcceptanceModal from './CallAcceptanceModal.tsx';
-import { SERVER_ADDRESS } from '../../../common/constants.ts';
+import { useDispatch } from 'react-redux';
 import { AnyAction } from 'redux';
 import { ThunkDispatch } from 'redux-thunk';
-import { useDispatch } from 'react-redux';
+import CallAcceptanceModal from './CallAcceptanceModal.tsx';
+import SignalRHandler from '../../../common/signalRHandler.ts';
+import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
+
+
 import { setInitialCallerUserName } from '../../../actions/videoActions/videoActions.ts';
 
-const SignalRHandler = ({ token, onAccept, onDecline }) => {
+const SignalRHandlerComponent = ({ token, onAccept, onDecline }) => {
     const [callReceived, setCallReceived] = useState(false);
     const [connection, setConnection] = useState<HubConnection | null>(null);
     const [showModal, setShowModal] = useState(false);
     const [key, setKey] = useState(0); // Add a key state to force re-render
-    const [callerConnectionid, setCallerConnectionId] = useState("")
+    const [callerConnectionId, setCallerConnectionId] = useState("");
 
     const dispatch = useDispatch<ThunkDispatch<any, any, AnyAction>>();
 
-    useEffect(() => {
-        const connect = new HubConnectionBuilder()
-            .withUrl(`${SERVER_ADDRESS}/hub?userToken=${encodeURIComponent(token)}&connectionType=${2}`)
-            .withAutomaticReconnect()
-            .withHubProtocol(new MessagePackHubProtocol())
-            .configureLogging(LogLevel.Information)
-            .build();
+    const signalRHandler = new SignalRHandler();
 
-        const startConnection = async () => {
+    useEffect(() => {
+        const initializeConnection = async () => {
             try {
-                await connect.start();
+                const connect = await signalRHandler.createSignalRConnection(2, token);
                 setConnection(connect);
             } catch (err) {
                 console.error('Error while establishing SignalR connection:', err);
             }
         };
 
-        startConnection();
-
-        const keepAliveInterval = setInterval(() => {
-            if (connect.state === 'Connected') {
-                connect.invoke("KeepAlive").catch(err => console.error("KeepAlive error:", err));
-            }
-        }, 15000); // Check and send KeepAlive every 15 seconds
-
-        connect.onclose(() => {
-            console.log("Connection lost. Attempting to reconnect...");
-        });
+        initializeConnection();
 
         return () => {
-            clearInterval(keepAliveInterval);
-            if (connect) {
-                connect.stop().then(() => console.log('SignalR connection closed'));
+            if (connection) {
+                signalRHandler.stopConnection(connection);
             }
         };
     }, [token]);
 
     useEffect(() => {
         if (connection && !callReceived) {
-            const handleAcceptCallRequest = (fromUser:string) => {
+            const handleAcceptCallRequest = (fromUser: string) => {
                 setShowModal(true);
-                setCallerConnectionId(fromUser)
+                setCallerConnectionId(fromUser);
                 setCallReceived(true);
             };
-            //Step 1: In the receiver showing call.
-            connection.on('acceptcallrequest', handleAcceptCallRequest);
-            return () => connection.off('acceptcallrequest', handleAcceptCallRequest);
+
+            // Step 1: In the receiver showing call.
+            signalRHandler.onConnectionEvent(connection, 'acceptcallrequest', handleAcceptCallRequest);
+
+            return () => signalRHandler.offConnectionEvent(connection, 'acceptcallrequest', handleAcceptCallRequest);
         }
     }, [connection, callReceived]);
 
@@ -72,13 +59,13 @@ const SignalRHandler = ({ token, onAccept, onDecline }) => {
         setShowModal(false);
 
         if (connection) {
-            //Step 2: In the receiver accepting call.
-            connection.send('AcceptCallFromReceiver');
+            // Step 2: In the receiver accepting call.
+            signalRHandler.sendMessageThroughConnection(connection, 'AcceptCallFromReceiver');
         }
 
         // Reset the component by updating the key
         setCallReceived(false);
-        dispatch(setInitialCallerUserName(callerConnectionid))
+        dispatch(setInitialCallerUserName(callerConnectionId));
         setKey(prevKey => prevKey + 1);
     };
 
@@ -105,4 +92,115 @@ const SignalRHandler = ({ token, onAccept, onDecline }) => {
     );
 };
 
-export default SignalRHandler;
+export default SignalRHandlerComponent;
+
+
+
+// import { HubConnection, HubConnectionBuilder, LogLevel } from '@microsoft/signalr';
+// import { MessagePackHubProtocol } from '@microsoft/signalr-protocol-msgpack';
+// import React, { useEffect, useState } from 'react';
+// import CallAcceptanceModal from './CallAcceptanceModal.tsx';
+// import { SERVER_ADDRESS } from '../../../common/constants.ts';
+// import { AnyAction } from 'redux';
+// import { ThunkDispatch } from 'redux-thunk';
+// import { useDispatch } from 'react-redux';
+// import { setInitialCallerUserName } from '../../../actions/videoActions/videoActions.ts';
+
+// const SignalRHandler = ({ token, onAccept, onDecline }) => {
+//     const [callReceived, setCallReceived] = useState(false);
+//     const [connection, setConnection] = useState<HubConnection | null>(null);
+//     const [showModal, setShowModal] = useState(false);
+//     const [key, setKey] = useState(0); // Add a key state to force re-render
+//     const [callerConnectionid, setCallerConnectionId] = useState("")
+
+//     const dispatch = useDispatch<ThunkDispatch<any, any, AnyAction>>();
+
+//     useEffect(() => {
+//         const connect = new HubConnectionBuilder()
+//             .withUrl(`${SERVER_ADDRESS}/hub?userToken=${encodeURIComponent(token)}&connectionType=${2}`)
+//             .withAutomaticReconnect()
+//             .withHubProtocol(new MessagePackHubProtocol())
+//             .configureLogging(LogLevel.Information)
+//             .build();
+
+//         const startConnection = async () => {
+//             try {
+//                 await connect.start();
+//                 setConnection(connect);
+//             } catch (err) {
+//                 console.error('Error while establishing SignalR connection:', err);
+//             }
+//         };
+
+//         startConnection();
+
+//         const keepAliveInterval = setInterval(() => {
+//             if (connect.state === 'Connected') {
+//                 connect.invoke("KeepAlive").catch(err => console.error("KeepAlive error:", err));
+//             }
+//         }, 15000); // Check and send KeepAlive every 15 seconds
+
+//         connect.onclose(() => {
+//             console.log("Connection lost. Attempting to reconnect...");
+//         });
+
+//         return () => {
+//             clearInterval(keepAliveInterval);
+//             if (connect) {
+//                 connect.stop().then(() => console.log('SignalR connection closed'));
+//             }
+//         };
+//     }, [token]);
+
+//     useEffect(() => {
+//         if (connection && !callReceived) {
+//             const handleAcceptCallRequest = (fromUser:string) => {
+//                 setShowModal(true);
+//                 setCallerConnectionId(fromUser)
+//                 setCallReceived(true);
+//             };
+//             //Step 1: In the receiver showing call.
+//             connection.on('acceptcallrequest', handleAcceptCallRequest);
+//             return () => connection.off('acceptcallrequest', handleAcceptCallRequest);
+//         }
+//     }, [connection, callReceived]);
+
+//     const handleModalAccept = () => {
+//         onAccept();
+//         setShowModal(false);
+
+//         if (connection) {
+//             //Step 2: In the receiver accepting call.
+//             connection.send('AcceptCallFromReceiver');
+//         }
+
+//         // Reset the component by updating the key
+//         setCallReceived(false);
+//         dispatch(setInitialCallerUserName(callerConnectionid))
+//         setKey(prevKey => prevKey + 1);
+//     };
+
+//     const handleModalDecline = () => {
+//         onDecline();
+//         setShowModal(false);
+
+//         // Reset the component by updating the key
+//         setCallReceived(false);
+//         setKey(prevKey => prevKey + 1);
+//     };
+
+//     return (
+//         <div key={key}>
+//             {showModal && (
+//                 <CallAcceptanceModal
+//                     onAccept={handleModalAccept}
+//                     onDecline={handleModalDecline}
+//                     onReceiveCall={() => {}}
+//                     token={token}
+//                 />
+//             )}
+//         </div>
+//     );
+// };
+
+// export default SignalRHandler;
