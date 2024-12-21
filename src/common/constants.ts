c19a31bbf3201ca196399ed6d3a6fca5fc9b6e3c
asold <asoldan1459@gmail.com>
2024-12-21 14:20:02 +0100

    changed the server address to the server on raspberry pi

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,3 +1,4 @@
-export const SERVER_ADDRESS = process.env.REACT_APP_SERVER_ADDRESS || 'http://localhost:8000';
+// export const SERVER_ADDRESS = process.env.REACT_APP_SERVER_ADDRESS || 'http://localhost:8000';
 // export const SERVER_ADDRESS = 'http://localhost:8000';
+export const SERVER_ADDRESS = 'https://mesharaserver.win';
 // export const SERVER_ADDRESS = 'https://tutoritbackend.azurewebsites.net';
