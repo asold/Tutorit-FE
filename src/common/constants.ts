44b918ec8a27c06c492822a2737b05da3b760a5c
asold <asoldan1459@gmail.com>
2025-09-02 17:37:38 +0200

    changed server location

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,3 +1,3 @@
-export const SERVER_ADDRESS = 'https://mesharaserver.win';
-// export const SERVER_ADDRESS = 'http://localhost:8000';
+// export const SERVER_ADDRESS = 'https://mesharaserver.win';
+export const SERVER_ADDRESS = 'http://localhost:8000';
 
